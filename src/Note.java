1c59cb3ea28f2a5e9b003e0070e21d010517a183
aynazmeymanat <aynazmeymanat@gmail.com>
2024-12-10 00:04:02 +0330

    cunstructor and getter added

--- src/Note.java
+++ src/Note.java
@@ -5,4 +5,22 @@ public class Note {
     private String content;
     private Date creationDate;
 
+    public Note(String name, String content) {
+        this.name = name;
+        this.content = content;
+        this.creationDate = new Date();
+    }
+
+    public String getName() {
+        return name  ;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
 }
